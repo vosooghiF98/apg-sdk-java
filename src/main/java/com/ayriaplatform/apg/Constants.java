f447f270f24d090511e92e23d9b9d3536b19f671
farshid <103681361+vosooghiF98@users.noreply.github.com>
2025-12-29 10:50:24 +0330

    Update Constants.java
    APG_API_URL was debugged

--- src/main/java/com/ayriaplatform/apg/Constants.java
+++ src/main/java/com/ayriaplatform/apg/Constants.java
@@ -2,7 +2,7 @@ package com.ayriaplatform.apg;
 
 public class Constants {
 
-    public static final String APG_API_URL = "https://api.ayria.club/apg/v1";
+    public static final String APG_API_URL = "https://api.ayriaclub.ir/apg/v1";
     public static final String APG_DEV_URL = "https://dev.ayria.club/apg/v1";
 
     public static final String APG_API_KEY = "APG-API-KEY";
